9fa6d0ce94749ee39559f7235956d429fd28d01a
alby11 <alby11@duck.com>
2025-03-06 10:08:26 +0100

    merged from markdown branch + added scritps to create pdf resume

---
+++ create_pdf.py
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+pandoc content.md -o resume.pdf \
+--pdf-engine=xelatex \
+-V geometry:margin=0.3in \
+-V mainfont="Liberation Sans" \
+-V fontsize=10pt \
+-V colorlinks=true
---
+++ create_pdf.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+pandoc content.md -o alberto_tallone_resume.pdf \
+  --pdf-engine=xelatex \
+  -V geometry:margin=0.3in \
+  -V mainfont="Liberation Sans" \
+  -V fontsize=10pt \
+  -V colorlinks=true
